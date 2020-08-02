b7bd12d0e5178db27e31102f87871937e1bcb915
Matt Haythornthwaite <m.haythornthwaite@yahoo.co.uk>
2020-08-02 19:08:26 +0100

    Dog Breed Classification Label Encoding

--- Dog_Breed_Image_Classification/Dog_breed_classification.py
+++ Dog_Breed_Image_Classification/Dog_breed_classification.py
@@ -11,7 +11,7 @@ print(' ---------------- START ---------------- \n')
 #------------------------------ KAGGLE DOG BREEDS -----------------------------
 
 #Mission Statement
-#Build an application that allows a user to upload a photo and return a label stating the breed of the dog in the photo. This will be interesting to see the results given an image with no dog, but thats to be investigated later. This will be avaluated against other submissions on kaggle
+#Build an application that allows a user to upload a photo and return a label stating the breed of the dog in the photo. This will be interesting to see the results given an image with no dog, but thats to be investigated later. This will be evaluated against other submissions on kaggle.
 
 #Data
 #Data has been downloaded from kaggle. https://www.kaggle.com/c/dog-breed-identification/overview. We have over 10,000 images in our train set (labelled) all of different sizes and resolutions, and we have 120 dog breeds to identify. Averages to around 80 images of each dog breed. N.B./ we have an additional ~10,000 images in our test set (with no labels).
@@ -19,7 +19,9 @@ print(' ---------------- START ---------------- \n')
 #Evaluation
 #Evaluation on kaggle requires a file with predicted probabilities of each dog breed for each test image. So there will be 120 predictions per image and the evaluation metric on kaggle is log loss
 
+from PIL import Image
 import pandas as pd
+import numpy as np
 import tensorflow as tf
 import tensorflow_hub as hub
 print('TF Version:', tf.__version__)
@@ -33,7 +35,43 @@ print("Num GPUs:", len(physical_devices))
 
 #------------------------------ DATA PREPARATION ------------------------------
 
+#loading our training labels and investigating number of labels per breed.
+training_labels = pd.read_csv('labels.csv')
+image_count = training_labels['breed'].value_counts()
+print(training_labels.head())
 
+#inspecting an image 
+test_image = Image.open('train/000bec180eb18c7604dcecc8fe0dba07.jpg')
+test_image.show()
+
+#creating list of ID's using lish comprehension so we can use filenames[n] to access an image
+filenames = ['train/' + fname + '.jpg' for fname in training_labels['id']]
+
+#accessing image with above list
+Image.open(filenames[9000]).show()
+print(training_labels['breed'][9000])
+
+
+#turning our labels from text to integers
+
+#creating list of dog breeds and assigning each one an integer
+labels_unique = pd.DataFrame({})
+labels_unique['id_str'] = np.unique(np.array(training_labels['breed']))
+labels_unique['id_int'] = list(range(1,121))
+
+#iterating over the entire dataframe of training labels and creating a list of the associated integer id.
+training_labels_id_int = []
+for i in range(0, len(training_labels)):
+    id_breed = training_labels['breed'][i]
+    for j in range(0,120):
+        id_int = labels_unique['id_int'][j]
+        id_str = labels_unique['id_str'][j]
+        if id_breed == id_str:
+            training_labels_id_int.append(id_int)
+            break
+
+#appending this list to the training labels dataframe
+training_labels['id_int'] = training_labels_id_int
 
 
 
