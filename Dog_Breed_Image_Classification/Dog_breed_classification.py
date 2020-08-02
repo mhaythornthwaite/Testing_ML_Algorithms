3ad8c2734e06ab3f6199e36ac162cb815425114d
Matt Haythornthwaite <m.haythornthwaite@yahoo.co.uk>
2020-08-02 14:26:28 +0100

    Dog Breed Classification TF Setup

--- Dog_Breed_Image_Classification/Dog_breed_classification.py
+++ Dog_Breed_Image_Classification/Dog_breed_classification.py
@@ -10,6 +10,29 @@ print(' ---------------- START ---------------- \n')
 
 #------------------------------ KAGGLE DOG BREEDS -----------------------------
 
+#Mission Statement
+#Build an application that allows a user to upload a photo and return a label stating the breed of the dog in the photo. This will be interesting to see the results given an image with no dog, but thats to be investigated later. This will be avaluated against other submissions on kaggle
+
+#Data
+#Data has been downloaded from kaggle. https://www.kaggle.com/c/dog-breed-identification/overview. We have over 10,000 images in our train set (labelled) all of different sizes and resolutions, and we have 120 dog breeds to identify. Averages to around 80 images of each dog breed. N.B./ we have an additional ~10,000 images in our test set (with no labels).
+
+#Evaluation
+#Evaluation on kaggle requires a file with predicted probabilities of each dog breed for each test image. So there will be 120 predictions per image and the evaluation metric on kaggle is log loss
+
+import pandas as pd
+import tensorflow as tf
+import tensorflow_hub as hub
+print('TF Version:', tf.__version__)
+print('TF Hub Version:', hub.__version__, '\n')
+
+#Check GPU
+physical_devices = tf.config.list_physical_devices('GPU')
+print('GPU', ' is available' if physical_devices else ' is not available')
+print("Num GPUs:", len(physical_devices))
+
+
+#------------------------------ DATA PREPARATION ------------------------------
+
 
 
 
