aa0d51559fc25baa3c56b1d4a90bbf1f9f56c5b8
Matt Haythornthwaite <m.haythornthwaite@yahoo.co.uk>
2022-01-26 22:07:11 +0000

    Eulers Number

--- Graphs_and_Functions/01_general_functions.py
+++ Graphs_and_Functions/01_general_functions.py
@@ -5,20 +5,42 @@ Created on Mon Jan 24 23:10:44 2022
 @author: mhayt
 """
 
-from math import exp
+from math import exp, e
 import matplotlib.pyplot as plt
 import numpy as np
 
 plt.close('all')
+
+def axis_spine_placement(ax, x=0, y=0):
+    ax.spines['left'].set_position(('data', x))
+    ax.spines['bottom'].set_position(('data', y))
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
+    ax.grid(c='#eeeeee')
+    
+#---------------------------------- y = e^x -----------------------------------
 
 x = list(np.linspace(-2, 2, 20))
 y = [exp(i) for i in x]
 
 fig, ax = plt.subplots()
 ax.plot(x, y)
+fig.suptitle('y=e^x', y=0.95, fontsize=16, fontweight='bold')
+axis_spine_placement(ax)
 
-#spine placement data centered
-ax.spines['left'].set_position(('data', 0.0))
-ax.spines['bottom'].set_position(('data', 0.0))
-ax.spines['right'].set_color('none')
-ax.spines['top'].set_color('none')
+
+#------------------------------ y = (1 + [1/x])^x -----------------------------
+
+x = list(np.linspace(0, 10, 25))
+y = [(1+(1/i))**i for i in x]
+
+fig, ax = plt.subplots()
+ax.plot(x, y)
+fig.suptitle('y=(1+(1/x))^x', y=0.95, fontsize=16, fontweight='bold')
+axis_spine_placement(ax)
+ax.set_ylim(0,3.5)
+
+ax.hlines(y=e, xmin=0, xmax=10, color='#FCCCBB')
+
+
+
